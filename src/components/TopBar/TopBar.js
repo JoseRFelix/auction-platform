58198e5cd70a18b4b3ba129557b11fe76f212ed2
jfelix61 <rtfire456@hotmail.com>
2019-05-09 00:35:09 -0400

    TopBar component is finished with its css

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -1,13 +1,24 @@
 import React from "react";
 import logo from "../../assets/logo.png";
+import "./TopBar.css";
 import { ReactComponent as PhoneIcon } from "../../assets/phone.svg";
+import { ReactComponent as UserIcon } from "../../assets/user.svg";
+import { ReactComponent as CaretIcon } from "../../assets/caret.svg";
 
 const TopBar = () => {
   return (
     <nav className="top-bar">
-      <div>
-        <img src={logo} />
-        <PhoneIcon />
+      <div className="top-bar__image-container">
+        <img className="top-bar__image" src={logo} alt="Logo" />
+        <div className="top-bar__divider">&#8203;</div>
+        <h3 className="top-bar__phone-number">
+          <PhoneIcon /> (11) 3569 - 3465
+        </h3>
+      </div>
+      <div className="user-box">
+        <UserIcon className="user-box__user-icon" />
+        <h3>User Test</h3>
+        <CaretIcon className="user-box__caret" />
       </div>
     </nav>
   );
